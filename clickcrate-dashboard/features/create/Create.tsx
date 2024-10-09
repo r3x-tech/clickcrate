879f954e62e57148afac1c07685e7570f988463d
adrian-petrykiw <cucumber901@gmail.com>
2024-10-09 14:12:14 -0500

    adjust

--- clickcrate-dashboard/features/create/Create.tsx
+++ clickcrate-dashboard/features/create/Create.tsx
@@ -86,7 +86,7 @@ export default function Create() {
         !error &&
         recentCreations &&
         recentCreations.length > 0 && (
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 max-h-[70vh] overflow-y-auto">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 max-h-[78vh] overflow-y-auto">
             {recentCreations.map((creation: RecentCreation) => (
               <div
                 key={creation.mintAddress}
